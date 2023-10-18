3b46325ab167b1575aaee0f7f390086ee47818d8
Thunder-11 <thunder-11@Admin-11s-iMac.local>
2023-10-18 21:28:42 +0800

    add:lesson day 14

--- lesson_day_14/lib/main.dart
+++ lesson_day_14/lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:word_find_app/start_screen.dart';
 
 import 'home_screen.dart';
 
 void main() {
   runApp(MaterialApp(
-      home: StartScreen()));
+    home: HomeScreen(
+    ),
+  ));
 }

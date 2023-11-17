b228b7321fec0635a613b9d8aae635141d020f17
Thunder-11 <thunder-11@Admin-11s-iMac.local>
2023-11-17 21:16:45 +0800

    add: lesson dayb 27

---
+++ icodegram/lib/gramhome.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class GramHome extends StatelessWidget {
+  const GramHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final _numberController = TextEditingController();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'iCodeGram',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: 'Lobster',
+                fontSize: 35,
+                fontWeight: FontWeight.w400,
+                color: Colors.white,
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(top: 52)),
+            Padding(padding: EdgeInsets.only(left: 15)),
+            TextField(
+              style: TextStyle(color: Colors.white),
+              controller: _numberController,
+              decoration: InputDecoration(
+                hintText: "Утасны дугаар",
+                hintStyle: TextStyle(color: Colors.white, fontSize: 14),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                      width: 3, color: Color(0xFFA0A0A0)), //<-- SEE HERE
+                ),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    Icons.clear,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {
+                    _numberController.clear();
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- icodegram/lib/icodegram.dart
+++ icodegram/lib/icodegram.dart
@@ -160,7 +160,7 @@ class IcodeGram extends StatelessWidget {
                   ),
                 ),
               ],
-            )
+            ),
           ],
         ),
       ),

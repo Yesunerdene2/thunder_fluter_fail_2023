61b1c56e409a3f5907f8c194e81cee9d3787512f
Thunder-11 <thunder-11@Admin-11s-iMac.local>
2023-12-01 20:13:08 +0800

    add: lesson day 33

---
+++ lesson_day_33_flutter/lib/utils.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+showSanckBar(BuildContext context, String text) {
+  return ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(content: Text(text),
+    ),);
+}
+
+pickImage(ImageSource source) async {
+  final ImagePicker picker = ImagePicker();
+  final XFile? image = await picker.pickImage(source: source);
+  if (image != null) {
+    return await image.readAsBytes();
+  }
+  print('No image is selected');
+}
